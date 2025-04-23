295d4a71c09407816440ac68eed2dc0edbe77d13
Kibugi-Erick <erickjohn497@gmail.com>
2025-04-23 14:57:36 +0300

    Update Practice.py

--- Practice.py
+++ Practice.py
@@ -1,7 +1,6 @@
 print("Hello World")
 print("My buds")
-<<<<<<< HEAD
-=======
 print("Data Science at Moringa")
 print("WRC 2025")
->>>>>>> e71ed29cf86dc12c4ce29b01115038e4cd52987f
+print("Another banger")
+
